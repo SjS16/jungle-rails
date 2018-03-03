4030621e5068b8989511dfa781eaf24d81c5971d
Stephanie Simpson <stephsimpson911@gmail.com>
2018-03-03 20:52:27 +0000

    error messages fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-      user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+      @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/users/new'
-      flash[:alert] = "There was something wrong with your login credentials. Please try again."
+      
+      # @user.errors.messages
+     
+      # puts @user.errors.messages
+      render :new
+      #redirect_to '/users/new'
+      
     end
   end
 
